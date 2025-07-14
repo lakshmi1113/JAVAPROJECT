23607632616661d15f8078de6b7fba261979c878
lakshmi1113 <lakshmi.sharavati@gmail.com>
2025-07-14 17:29:22 +0530

    jsva

--- StudentDetails.java
+++ StudentDetails.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class StudentDetails {
+public class StudentDetails{
     public static int n;
     public static Student[] s = new Student[n];
     public static void main(String[]args){
@@ -10,15 +10,19 @@ public class StudentDetails {
        if (ch == 1){
         System.out.println("Enter the no.of students you would like to add");
         n = sc.nextInt();
-        for(int i = 1; i<n; i++){
+        System.out.println(s[0]);
+        for(int i = 0; i<n; i++){
+            System.out.println(i);
                 System.out.println("enter student"+i+"s rollno.");
                 int rno= sc.nextInt();
+                sc.nextLine();
                 System.out.println("enter student"+i+"s name");
                 String nme = sc.nextLine();
                 System.out.println("enter student" + i + "s address");
                 String adr = sc.nextLine();
                 System.out.println("enter student" + i + "s grade");
                 int grd = sc.nextInt();
+                sc.nextLine();
                 System.out.println("enter student" + i + "s date of birth");
                 String dob = sc.nextLine();
                 System.out.println("enter student" + i + "s marks percentage");
@@ -29,13 +33,42 @@ public class StudentDetails {
     else if(ch == 2){
         System.out.println("Enter the rollno. of the student(whose details to be modified)");
         int r = sc.nextInt();
-        System.out.println("Enter which variable must be modified(in lowercase letter)");
-        String vm = sc.nextLine();
-        for(int j =1; j<=n; j++){
-            if(s[j].rollno == r){
-                
+        System.out.println("which of the three would you like to modify:-\n1.grade\n2.address\n3.percentage");
+        int vm = sc.nextInt();
+        if(vm==1){
+            System.out.println("enter to what it should change to:-");
+            int mval = sc.nextInt();
+            for (int j = 1; j <= n; j++) {
+                if (s[j].rollno == r) {
+                    s[j].grade = mval;
+                }
             }
         }
+        else if(vm==2){
+            System.out.println("enter to what it should change to:-");
+            String mval = sc.nextLine();
+            for (int j = 1; j <= n; j++) {
+                if (s[j].rollno == r) {
+                    s[j].address = mval;
+                }
+            }
+        }
+        else if(vm==3){
+            System.out.println("enter to what it should change to:-");
+            float mval = sc.nextFloat();
+            for (int j = 1; j <= n; j++) {
+                if (s[j].rollno == r) {
+                    s[j].percentage = mval;
+                }
+            }
+        }
+        else{
+            System.out.println("Invalid Input");
+        }
+        
+    }
+    else if(ch ==3){
+
     }
 }
 }
